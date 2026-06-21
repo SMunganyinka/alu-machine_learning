e06f39044ef86062b2a2f4c1372ec2f8cee60737
smunganyinka <s.munganyin@alustudent.com>
2026-06-21 15:04:32 -0700

    6

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,58 +1,69 @@
 #!/usr/bin/env python3
-
 """
-poisson distribution
+Poisson distribution module
 """
 
 
-class Poisson():
+class Poisson:
     """
-    poisson distribution
+    Represents a Poisson distribution
     """
+
     def __init__(self, data=None, lambtha=1.):
         """
-        data is a list of the data to be used to estimate the distribution
-        lambtha is the expected number of occurences in a given interval
+        Initialize Poisson distribution
         """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
 
-        if data is not None:
+        else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+
             self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
         """
-        Calculates the value of the PMF for a given number of “successes”.
+        Calculates the value of the PMF for a given k
         """
         if not isinstance(k, int):
             k = int(k)
+
         if k < 0:
             return 0
+
         e = 2.7182818285
         factorial = 1
+
         for i in range(1, k + 1):
             factorial *= i
-        return ((e ** (-self.lambtha)) * (self.lambtha ** k)) / factorial
+
+        return ((e ** (-self.lambtha)) *
+                (self.lambtha ** k)) / factorial
 
     def cdf(self, k):
         """
-        Calculates the value of the CDF for a given number of “successes”.
+        Calculates the value of the CDF for a given k
         """
         if not isinstance(k, int):
             k = int(k)
+
         if k < 0:
             return 0
+
         e = 2.7182818285
         summation = 0
+
         for i in range(k + 1):
             factorial = 1
+
             for j in range(1, i + 1):
                 factorial *= j
+
             summation += (self.lambtha ** i) / factorial
+
         return (e ** (-self.lambtha)) * summation
